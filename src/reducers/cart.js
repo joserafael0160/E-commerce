0ab59fc6739bd9119b6c98d51b712327c90ac269
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-08-04 17:59:12 -0400

    Adding localStorage

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,5 +1,9 @@
 
-export const cartInitialState = [] 
+export const cartInitialState = JSON.parse(localStorage.getItem("cart")) || []
+
+export const updateLocalStorage = state => {
+  window.localStorage.setItem("cart", JSON.stringify(state))
+}
 
 export const cartReducer = (state, action) => {
   const { type: actionType, payload: actionPayload } = action
@@ -12,23 +16,29 @@ export const cartReducer = (state, action) => {
       if(productInCartIndex >= 0) {
         const newState = structuredClone(state)
         newState[productInCartIndex].quantity += 1
+        updateLocalStorage(newState)
         return newState
       }
-
-      return [
+      const newState = [
         ...state,
         {
           ...actionPayload,
           quantity: 1
         }
       ]
+      
+      updateLocalStorage(newState)
+      return newState
     }
     case "REMOVE_FROM_CART": {
       const { id } = actionPayload
-      return state.filter(item => item.id !== id)
+      const newState = state.filter(item => item.id !== id)
+      updateLocalStorage(newState)
+      return newState
     }
     case "CLEAR_CART": {
-      return cartInitialState
+      updateLocalStorage([])
+      return []
     }
   }
 }
